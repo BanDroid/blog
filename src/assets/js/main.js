7f5e3b9f86ae8c721f268a891e4ce97e31d7fd80
BanDroid <dbbhuh5@gmail.com>
2024-07-19 13:31:23 +0000

    fix: same thing

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -61,7 +61,8 @@ document.addEventListener(
     menu.classList.add("!duration-300");
     document.getElementById("header").classList.add("!duration-300");
     if (location.pathname.startsWith("/search")) {
-      searchBox.classList.add("hidden md:hidden");
+      searchBox.classList.add("hidden");
+      searchBox.classList.add("md:hidden");
     }
   },
   false
