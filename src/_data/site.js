9e1f6390cd72258103f1d31272d794d4b35d2cf0
Fitrah Nuno Syahbani <68449470+BanDroid@users.noreply.github.com>
2024-07-21 18:07:26 +0700

    feat: add disqus feature
    use DISQUS_SHORTNAME env

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,5 +5,5 @@ module.exports = {
   url: process.env.RENDER_EXTERNAL_URL || process.env.BASE_URL || "", // your site url without trailing slash
   paginate: 6, // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
-  // disqusShortname: "your-shortname"
+  disqusShortname: process.env.DISQUS_SHORTNAME
 };
